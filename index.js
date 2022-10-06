8a955e9b75cfe2a555dbda8e1e57099956e7f53e
abdallah-nour <abdallah.nouraldin@gmail.com>
2022-10-06 22:03:40 +0300

    add other ways to check for duplicates

--- index.js
+++ index.js
@@ -13,11 +13,34 @@ const containsDuplicate2 = (arr = []) => {
   return false;
 };
 
-console.log('has double?', containsDuplicate2([2, 3, 4, 1]));
-console.log('has double?', containsDuplicate2([2, 3, 4, 1, 5, 6, 7]));
-console.log('has double?', containsDuplicate2([1, 5, -2, -4, 0]));
+const containsDuplicate3 = (arr = []) => {
+  const set = new Set(arr);
+  return set.size === arr.length ? false : true;
+};
 
-// const containsDuplicate3 = (arr = []) => {
+const containsDuplicate4 = (arr = []) => {
+  const map = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]]) return true;
+    map[arr[i]] = true;
+  }
+  return false;
+};
+
+const containsDuplicate5 = (arr = []) => {
+  const hashTable = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    if (hashTable.has(arr[i])) return true;
+    else hashTable.set(arr[i], true);
+  }
+  return false;
+};
+
+console.log('has double?', containsDuplicate4([2, 3, 4, 1]));
+console.log('has double?', containsDuplicate4([2, 3, 4, 1, 5, 6, 7]));
+console.log('has double?', containsDuplicate4([1, 5, -2, -4, 0]));
+
+// const containsDuplicate4 = (arr = []) => {
 //   const strArr = ',' + arr.join(',');
 //   let j = 1;
 //   for (let i = 0; i < arr.length; i++, j += 2) {
